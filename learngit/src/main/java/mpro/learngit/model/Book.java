b4d856c695acf31daf164b33b9720f0c2db114b5
Surya <suryajayant@yahoo.com>
2017-11-22 13:39:23 +0530

    Book Bean created

---
+++ learngit/src/main/java/mpro/learngit/model/Book.java
@@ -0,0 +1,25 @@
+package mpro.learngit.model;
+
+public class Book {
+	private int bookId;
+	private String bookName;
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public Book(int bookId, String bookName) {
+		super();
+		this.bookId = bookId;
+		this.bookName = bookName;
+	}
+	
+	
+}
